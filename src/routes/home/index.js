f4459e84e57096a7c0752b6f191fea973a251bc9
seunbayo <seunbayo83@gmail.com>
2022-09-01 02:19:23 +0100

    inital file demo

---
+++ src/routes/home/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import style from './style.css';
+
+const Home = () => (
+	<div class={style.home}>
+		<h1>Spheron: The home of DAPPS</h1>
+		<p>This is a demo preact Application that is Hosted on Spheron.network</p>
+	</div>
+);
+
+export default Home;
